9e01d0632bcd729ab143eff6b717980b2cb11716
timelessnesses <mooping3roblox@gmail.com>
2022-11-08 21:21:53 +0700

    help

--- sql/starter.sql
+++ sql/starter.sql
@@ -3,4 +3,20 @@ CREATE TABLE IF NOT EXISTS config(
     annouce_channel_id BIGINT NOT NULL,
     timezone VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS reminder_message(
+    id BIGINT NOT NULL PRIMARY KEY,
+    guild_id BIGINT NOT NULL,
+    channel_id BIGINT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS new_year_message(
+    id BIGINT, -- this is optional cus of uhhh we also store how message format is and stuff and yeah plus this is just for deleting purposes
+    guild_id BIGINT NOT NULL PRIMARY KEY,
+    channel_id BIGINT NOT NULL,
+    message_format TEXT,
+    ping_role_id BIGINT,
+    body_message_format TEXT
+);
+
 -- lol
--- src/new_year.py
+++ src/new_year.py
@@ -1,6 +1,8 @@
+import typing
 import discord
 from discord.ext import commands, tasks
-
+import pytz
+from datetime import datetime
 import sql.sql as sql
 
 
@@ -14,7 +16,7 @@ class New_Year(commands.Cog):
         self.db: sql.EasySQL = bot.db
         self.check_guild.start()
 
-    @tasks.loop(seconds=60)
+    @tasks.loop(hours=24)
     async def check_guild(self):
         all_guilds = await self.db.fetch("SELECT * FROM config")
         await self.bot.wait_until_ready()
@@ -27,3 +29,62 @@ class New_Year(commands.Cog):
                         description="Bot won't works if you don't setup bot!",
                     )
                 )
+                continue
+    
+    @tasks.loop(minutes=5)
+    async def check_time(self):
+        await self.bot.wait_until_ready()
+        now = datetime.now()
+        for guild in self.bot.guild:
+            guild: discord.Guild # mf type hinting
+            db_guild = await self.db.fetch("SELECT * FROM config WHERE guild_id = $1", guild.id)
+            reminder_message = await self.db.fetch("SELECT * FROM reminder_mesage WHERE guild_id = $1")
+            reminder_message = await guild.get_channel(reminder_message["channel_id"]).fetch_message(reminder_message["message_id"])
+            new_year_message = await self.db.fetch("SELECT * FROM new_year_message WHERE guild_id = $1")
+            new_year_message = guild.get_channel(new_year_message["channel_id"]) 
+            try:
+                current_timezone = pytz.timezone(db_guild["timezone"].title())
+            except pytz.exceptions.UnknownTimeZoneError:
+                current_timezone = pytz.timezone("UTC")
+            current_time = now.astimezone(current_timezone)
+            new_year_time = datetime(current_time.year, 1, 1, 0, 0, 0, 0, current_timezone)
+            estimated_left = new_year_time - current_time
+            if estimated_left.total_seconds() in range(0,86400*2): # new year is here and it lasts for 2 days after that 
+                channel = guild.get_channel(db_guild["annouce_channel_id"])
+                j = await channel.send(
+                    f"{(guild.get_role(new_year_message['ping_role_id']).mention if new_year_message['ping_role_id'] else '@everyone')}",
+                    embed=discord.Embed(
+                        title=f"{new_year_message['message_format'] if new_year_message['message_format'] else 'Happy new year! 🎉'}",
+                        description=f"{new_year_message['body_message_format'] if new_year_message['body_message_format'] else f'Happy new year! I hope you have a great year ahead! Welcome to {new_year_time.year}!'}",
+                        color=discord.Color.green(),
+                    )
+                )
+                await self.db.execute(
+                    "INSERT INTO new_year_message(id) VALUES ($1) WHERE guild_id = $2",
+                    j.id,
+                    guild.id
+                )
+            else:
+                # delete annouced message then count the time left
+                try:
+                    new_year_message.get_partial_message(new_year_message["message_id"]).delete()
+                except:
+                    pass
+                await reminder_message.edit(
+                    embed=(discord.Embed(
+                        title="New year is coming!",
+                        description=f"{self.format_time_to_string(estimated_left)} left until new year!",
+                        color=discord.Color.green(),
+                    )).set_footer(text=f"This message is controlled by timezone that's configured by the owner: {current_timezone.zone}")
+                )
+    
+    @commands.hybrid_group()
+    async def new_year(self, ctx: commands.Context):
+        """
+        New year group command
+        """
+        if ctx.invoked_subcommand is None:
+            await ctx.send_help(ctx.command)
+
+async def setup(bot: commands.Bot) -> typing.NoReturn:
+    await bot.add_cog(New_Year(bot))
